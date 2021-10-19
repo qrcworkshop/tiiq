4d847e6ea7804dcf46468629bedf870ad442bd25
David Fuentes Ruiz <trapeya25@gmail.com>
2021-10-19 13:05:54 +0200

    update file names update SGS100A class methods

--- src/tiiq/instruments/tests/test_SGS100A.py
+++ src/tiiq/instruments/tests/test_SGS100A.py
@@ -1,6 +1,6 @@
-from qiboicarusq.instruments_TII.RohdeSchwarz_SGS100A import RohdeSchwarz_SGS100A
+from tiiq.instruments.rohde_schwarz import SGS100A
 
-LO_qrm = RohdeSchwarz_SGS100A("LO_qrm", 'TCPIP0::192.168.0.7::inst0::INSTR')
+LO_qrm = SGS100A("LO_qrm", '192.168.0.7')
 LO_qrm.set_power(15)
 LO_qrm.set_frequency(7.789e9)
 LO_qrm.on()
@@ -10,7 +10,7 @@ print (LO_qcm.getIP())
 print (LO_qcm.getPower())
 print (LO_qcm.getFrequency())
 
-LO_qcm = RohdeSchwarz_SGS100A("LO_qcm", 'TCPIP0::192.168.0.8::inst0::INSTR')
+LO_qcm = SGS100A("LO_qcm", '192.168.0.8')
 LO_qcm.set_power(12)
 LO_qcm.set_frequency(8.9e9)
 LO_qcm.on()
